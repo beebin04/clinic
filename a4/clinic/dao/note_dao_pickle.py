e3ba29c3fb66d04a530834cde2e549b5b12614ad
Noah Ord <noahord@uvic.ca>
2024-11-19 10:55:13 -0800

    On branch master  Changes to be committed: 	modified:   note_dao.py 	modified:   note_dao_pickle.py 	modified:   patient_dao.py 	modified:   patient_dao_json.py 	modified:   patient_decoder.py
    	Added comments to each function in these files

--- a4/clinic/dao/note_dao_pickle.py
+++ a4/clinic/dao/note_dao_pickle.py
@@ -2,13 +2,14 @@ from .note_dao import NoteDAO
 from pickle import load, dump
 from clinic.note import Note
 class NoteDAOPickle(NoteDAO):
-    
+    #initialization block    
     def __init__(self, phn : int = None, autosave=False):
         self.autosave = autosave
         self.filename : str = f'clinic/records/{phn}.dat'
         self.notecounter = 0
         self.note_record = []
         if self.autosave:
+            #here we try to open the note file
             try:
                 with open(self.filename, 'rb') as file:
                     note_data = load(file)
@@ -19,13 +20,13 @@ class NoteDAOPickle(NoteDAO):
                 self.note_record = []
         else:
             self.note_record = []
-            
+    #searches for a note with a key and returns it
     def search_note(self, key) -> Note:
         if key > 0:
             for n in self.note_record:
                 if n.code == key:
                     return n
-        
+    #creates a new not containing the input as text, and returns it (note gets assigned an auto incremented counter value)
     def create_note(self, text):
         if text != None:
             self.notecounter += 1
@@ -36,14 +37,14 @@ class NoteDAOPickle(NoteDAO):
                     dump([n.to_dict() for n in self.note_record], file)
             return new_note
         return None
-
+    #searches for any notes containing the input string and returns them as a list
     def retrieve_notes(self, search_string):
         li = []
         for note in self.note_record:
             if search_string in note.text:
                 li.append(note)
         return li
-
+    #searches for a note with a key, and updates the text with the second input field
     def update_note(self, key, text):
         note = self.search_note(key)
         if note:
@@ -53,7 +54,7 @@ class NoteDAOPickle(NoteDAO):
                     dump([n.to_dict() for n in self.note_record], file)
             return True
         return False
-
+    #searches for a note with a key and deletes it from the record
     def delete_note(self, key):
         note = self.search_note(key)
         if note:
@@ -64,7 +65,7 @@ class NoteDAOPickle(NoteDAO):
                     dump([n.to_dict() for n in self.note_record], file)
             return True
         return False
-
+    #returns a list of all notes in the record
     def list_notes(self) -> list:
         li = []
         for note in self.note_record:
